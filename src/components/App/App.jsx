c4e529db93ed3e90a164bae5248a1c9ec1171b81
Ryan B <bombingrun3@gmail.com>
2025-06-05 19:59:39 -0500

    Initial Setup

---
+++ src/components/App/App.jsx
@@ -0,0 +1,14 @@
+import { HashRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route></Route>
+      </Routes>
+    </HashRouter>
+  );
+}
+
+export default App;
